82a054e50f6d1826d767779384952fa08f6ac8d8
Vojta Oupicky <voj.oup@gmail.com>
2019-03-05 07:00:27 +0100

    Initial commit

---
+++ vscode.js
@@ -0,0 +1,40 @@
+{
+  "workbench.colorTheme": "Cobalt2",
+  "window.zoomLevel": 0,
+  "editor.fontFamily": "Operator Mono Lig, Menlo, Monaco, 'Courier New', monospace",
+  "editor.fontSize": 16,
+  "editor.tabSize": 2,
+  "editor.fontWeight": "400",
+  "emmet.includeLanguages": {
+    "javascript": "javascriptreact"
+  },
+  "editor.formatOnSave": true,
+  "eslint.autoFixOnSave": true,
+  "eslint.alwaysShowStatus": true,
+  "workbench.iconTheme": "material-icon-theme",
+  "files.watcherExclude": {
+    "**/.git/objects/**": true,
+    "**/.git/subtree-cache/**": true,
+    "**/node_modules/*/**": true
+  },
+  "editor.fontLigatures": true,
+  "files.autoSave": "onFocusChange",
+  "cSpell.userWords": [
+    "Chartkick",
+    "Gatsbyjs",
+    "Gatsbys",
+    "browserslist",
+    "contentful",
+    "frontmatter",
+    "fullstack",
+    "graphiql",
+    "netlify",
+    "repos",
+    "vojoup",
+    "xtitle",
+    "ytitle"
+  ],
+  "terminal.integrated.rendererType": "dom",
+  "git.autofetch": true,
+  "javascript.updateImportsOnFileMove.enabled": "always"
+}
