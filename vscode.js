ca6989dabcb9e741b0799a16ef7ef32d413790b6
Vojta Oupicky <voj.oup@gmail.com>
2019-05-06 15:15:29 +0200

    Update vscode.js

--- vscode.js
+++ vscode.js
@@ -3,7 +3,7 @@
   "window.zoomLevel": 0,
   "editor.fontFamily": "Operator Mono Lig, Menlo, Monaco, 'Courier New', monospace",
   "editor.fontSize": 16,
-  "editor.tabSize": 2,
+  "editor.tabSize": 4,
   "editor.fontWeight": "400",
   "emmet.includeLanguages": {
     "javascript": "javascriptreact"
@@ -19,22 +19,16 @@
   },
   "editor.fontLigatures": true,
   "files.autoSave": "onFocusChange",
-  "cSpell.userWords": [
-    "Chartkick",
-    "Gatsbyjs",
-    "Gatsbys",
-    "browserslist",
-    "contentful",
-    "frontmatter",
-    "fullstack",
-    "graphiql",
-    "netlify",
-    "repos",
-    "vojoup",
-    "xtitle",
-    "ytitle"
-  ],
   "terminal.integrated.rendererType": "dom",
   "git.autofetch": true,
-  "javascript.updateImportsOnFileMove.enabled": "always"
+  "javascript.updateImportsOnFileMove.enabled": "always",
+  "javascript.validate.enable": false,
+  "editor.minimap.renderCharacters": false,
+  "editor.minimap.maxColumn": 200,
+  "editor.minimap.showSlider": "always",
+  "editor.smoothScrolling": true,
+  "editor.cursorBlinking": "phase",
+  "editor.cursorSmoothCaretAnimation": true,
+  "files.insertFinalNewline": true,
+  "files.trimTrailingWhitespace": true,
 }
